a29efac939cb9af20b1328be34f407db8df68c7a
Saidbekman <htaypirov4@gmail.com>
2025-05-20 15:38:50 +0500

    Create home.py

---
+++ lesson-1/homework/home.py
@@ -0,0 +1,24 @@
+# Homework:
+
+# Given a side of square. Find its perimeter and area.
+a=5
+print(pow(a,2))
+print(a*4)
+
+# Given diameter of circle. Find its length.
+b=5
+print(b*3.14)
+
+# Given two numbers a and b. Find their mean.
+a = 5
+b = 5
+mean = (a + b) / 2
+print("Mean:", mean)
+
+# Given two numbers a and b. Find their sum, product and square of each number.
+a = 5
+b = 5
+print(a+b)
+print(a*b)
+print(pow(a,2))
+print(pow(b,2))
